210bb4a21f13b8b1a5075cfa4c8588db4f8a1519
kinderhead <kinderhead512@gmail.com>
2022-10-07 13:41:13 -0400

    oo

---
+++ README.md
@@ -0,0 +1,8 @@
+# How Old are Rocks?
+
+A game showing couple ways of dating rocks.
+
+## Features
+
+* Radioactive dating
+* Using fossils for relative dating
---
+++ scripts/Rock.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class Rock : Polygon2D
+{
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(float delta)
+    {
+        
+    }
+}
