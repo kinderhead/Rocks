af9379dae3e3c08238256bf6081cc64bcfa1fdf1
kinderhead <kinderhead512@gmail.com>
2022-10-10 11:13:32 -0400

    boo

--- scripts/Rock.cs
+++ scripts/Rock.cs
@@ -1,11 +1,14 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Rock : Polygon2D
 {
     public override void _Ready()
     {
-        
+        var points = new List<Vector2>();
+
+        points.Add(new Vector2(GD.RandRange(-250, -500), 0));
     }
 
     public override void _Process(float delta)
