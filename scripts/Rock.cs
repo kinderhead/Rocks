e2984508831157e2d0566061bdf17135a0e82430
kinderhead <kinderhead512@gmail.com>
2022-10-11 18:39:56 -0400

    whoops

--- scripts/Rock.cs
+++ scripts/Rock.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 
 public class Rock : Polygon2D
 {
+    [Export]
+    public Color[] Colors = {};
+
     public override void _Ready()
     {
-        var points = new List<Vector2>();
-
-        points.Add(new Vector2(GD.RandRange(-250, -500), 0));
+        Color = Colors[new Random().Next(Colors.Length)];
     }
 
     public override void _Process(float delta)
