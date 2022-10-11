7e5476101a30e63ce4720893730664967465bf99
kinderhead <kinderhead512@gmail.com>
2022-10-11 19:15:10 -0400

    roc

--- scripts/Rock.cs
+++ scripts/Rock.cs
@@ -2,10 +2,17 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
+public enum RockTypes {
+    RADIOACTIVE
+}
+
 public class Rock : Polygon2D
 {
     [Export]
     public Color[] Colors = {};
+
+    [Export]
+    public RockTypes Type = RockTypes.RADIOACTIVE;
 
     public override void _Ready()
     {
